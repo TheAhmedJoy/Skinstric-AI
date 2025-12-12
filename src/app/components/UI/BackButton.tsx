b4fa03f73db11f14b2e914f7174e3988dd8f0977
Ahmed Ali <theahmedjoyali@gmail.com>
2025-12-11 17:21:34 -0800

    Updated file structure to src/app,  created button components and the layout of the home page for phase 1 use

---
+++ src/app/components/UI/BackButton.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import ButtonLeft from "./assets/button-icon-shrunk-left.svg"
+
+export default function BackButton() {
+    return (
+        <div>
+            <div className="relative w-12 h-12 flex items-center justify-center border 
+            border-[#1A1B1C] rotate-45 scale-[1] sm:hidden">
+                <span className="-rotate-45deg text-xs font-semibold sm:hidden">
+                    BACK
+                </span>
+            </div>
+            <div className="group hidden sm:flex flex-row relative justify-center items-center">
+                <Image className="group-hover:scale-110 duration-300" src={ButtonLeft} alt="Left button" />
+                <span className="text-sm font-semibold hidden sm:block ml-6 ">
+                    BACK
+                </span>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/components/UI/ProceedButton.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import ButtonRight from "./assets/button-icon-shrunk-right.svg"
+
+export default function ProceedButton() {
+    return (
+        <div>
+            <div className=" w-12 h-12 flex items-center justify-center border 
+            border-[#1A1B1C] rotate-45 scale-[1] sm:hidden">
+                <span className="-rotate-45deg text-xs font-semibold sm:hidden">
+                    PROCEED
+                </span>
+            </div>
+            <div className="group hidden sm:flex flex-row relative justify-center items-center">
+                <span className="text-sm font-semibold hidden sm:block mr-5">
+                    PROCEED
+                </span>
+                <Image className="group-hover:scale-110 duration-300" src={ButtonRight} alt="Right button" />
+            </div>
+        </div>
+    )
+}
