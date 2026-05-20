75bc9edef395bc815d4b5425190378a8e737682d
Ahmed Ali <theahmedjoyali@gmail.com>
2026-05-20 13:23:05 -0700

    Updated the SelectOptions to enable the Cosmetic Concerns button and route to the cosmetic-concerns page

--- src/app/components/UI/SelectOptions.tsx
+++ src/app/components/UI/SelectOptions.tsx
@@ -13,8 +13,8 @@ export default function SelectOptions() {
     return (
         <div className="relative">
             <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-                <div className={`absolute transition-all duration-400 
-                    ${hoveredButton === "demographics" ? "w-[602px] h-[602px] opacity-100" : "w-[400px] h-[400px] opacity-0"}`}>
+                <div className={`absolute transition-all duration-400
+                    ${hoveredButton === "demographics" || hoveredButton === "cosmetic" ? "w-[602px] h-[602px] opacity-100" : "w-[400px] h-[400px] opacity-0"}`}>
                     <Image src={RectangleWholeLayer1} alt="Rectangle Layer 1" />
                 </div>
             </div>
@@ -33,13 +33,15 @@ export default function SelectOptions() {
                 </div>
 
                 <div className="flex items-center justify-center row-start-2 col-start-1">
-                    <button className="w-[153.88px] h-[153.88px] bg-gray-100 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 
-                        font-semibold leading-6 tracking-tight uppercase cursor-not-allowed"
-                        onMouseEnter={() => setHoveredButton("cosmetic")} onMouseLeave={() => setHoveredButton(null)}>
-                        <span className="transform -rotate-45">
-                            Cosmetic Concerns
-                        </span>
-                    </button>
+                    <Link href="/cosmetic-concerns">
+                        <button className="w-[153.88px] h-[153.88px] bg-gray-200 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5
+                            cursor-pointer font-semibold leading-6 tracking-tight uppercase hover:scale-[1.05] transition-transform duration-300"
+                            onMouseEnter={() => setHoveredButton("cosmetic")} onMouseLeave={() => setHoveredButton(null)}>
+                            <span className="transform -rotate-45">
+                                Cosmetic Concerns
+                            </span>
+                        </button>
+                    </Link>
                 </div>
 
                 <div className="flex items-center justify-center row-start-2 col-start-3">
